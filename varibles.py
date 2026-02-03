053469b90a77974b4028096f45febd9b1c20d04a
Muhammed Labeeb <labeebm003@gmail.com>
2026-02-03 12:57:42 +0530

    Add files via upload

---
+++ varibles.py
@@ -0,0 +1,21 @@
+#strings
+first_name ="John"
+print(first_name)
+print(f"hello {first_name}")
+food="apple"
+print(food)
+print(f"i like {food}")
+#integers
+age=23
+num=5567893444
+qun=6
+print(f"my age is {age} and my num is {num} and i have {qun} cars ")
+#float
+price=3.99
+print(f"my apple price is {price}")
+# boolean
+is_std=False
+if is_std:
+    print("i am std")
+else:
+        print("i am not std")
